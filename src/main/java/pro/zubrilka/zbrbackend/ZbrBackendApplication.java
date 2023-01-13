af915f4daca751c6e3a5b2d4b78d96786edff663
leshuk <89050170077@mail.ru>
2023-01-13 12:22:17 +0400

    basic: basic non-functional commit

---
+++ src/main/java/pro/zubrilka/zbrbackend/ZbrBackendApplication.java
@@ -0,0 +1,13 @@
+package pro.zubrilka.zbrbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZbrBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZbrBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/zubrilka/zbrbackend/ZbrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.zubrilka.zbrbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZbrBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
